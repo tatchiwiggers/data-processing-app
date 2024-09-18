291db97e214be8596f02aecc0f1e9a43a40e655a
tabatha wiggers <tatchiwiggers@gmail.com>
2024-09-18 22:53:45 +0000

    commit for lu

--- sql/snowflake/create_database.sql
+++
@@ -1,3 +0,0 @@
-CREATE OR REPLACE DATABASE ecommerce_db
-COMMENT = 'Database for E-commerce product and sales data'
-DATA_RETENTION_TIME_IN_DAYS = 1;
--- sql/snowflake/create_warehouse.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE WAREHOUSE ecommerce_wh
-WITH WAREHOUSE_SIZE = 'XSMALL' -- Specify size, e.g., XSMALL, SMALL, MEDIUM, etc.
-AUTO_SUSPEND = 300             -- Suspend warehouse after 300 seconds of inactivity
-AUTO_RESUME = TRUE;            -- Automatically resume the warehouse when a query is executed
