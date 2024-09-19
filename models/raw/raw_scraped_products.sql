7b07fc3a99aa5130992541140d793b7a685a1e2b
Tabatha Wiggers <tatchisabato@gmail.com>
2024-09-19 18:04:16 +0000

    adds cleaned products transformations from ml web scraping

---
+++ models/raw/raw_scraped_products.sql
@@ -0,0 +1,11 @@
+SELECT
+    product_id,
+    product_link,
+    product_image,
+    product_title,
+    previous_price,
+    current_price,
+    discount,
+    installments,
+    seller
+FROM ecommerce_db.ecommerce.scraped_products
